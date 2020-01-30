f5540fbfbd1a102f1ba98bedbf7d7a966922a0b2
Darrell Root <darrellrootjunk@mac.com>
2020-01-30 14:28:03 -0800

    Initial Commit

---
+++ Etherdump/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Etherdump
+//
+//  Created by Darrell Root on 1/30/20.
+//  Copyright © 2020 net.networkmom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
