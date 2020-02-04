060b67fa357260e715d520ec1a3b9917ce6d76f1
Darrell Root <darrellrootjunk@mac.com>
2020-02-04 09:05:58 -0800

    commit before trying to import file

--- Etherdump/AppDelegate.swift
+++ Etherdump/AppDelegate.swift
@@ -34,6 +34,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func importPcapngFile(_ sender: NSMenuItem) {
+        let contentView = ContentView()
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
 
+    }
+    
 }
 
