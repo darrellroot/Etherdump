75fd4a7c6c143876c80ec35444252fab89deef3c
Darrell Root <darrellrootjunk@mac.com>
2020-02-07 12:56:52 -0800

    Added capture filter controls

--- Etherdump/CaptureFilterView.swift
+++ Etherdump/CaptureFilterView.swift
@@ -13,31 +13,51 @@ import PackageEtherCaptureC
 struct CaptureFilterView: View {
     @State var etherCapture: EtherCapture? = nil
     @Binding var frames: [Frame]
+    @State var captureFilter: String = ""
+    @State var error: String = ""
+    @State var numberPackets = 10
     var body: some View {
         HStack {
             Button("Start") {
                 self.startButtonPress()
             }
-            Button("Stop") {
-                
+            Picker(selection: $numberPackets, label: Text("")) {
+                Text("Capture 1 Packet").tag(1)
+                Text("Capture 10 Packets").tag(10)
+                Text("Capture 100 Packets").tag(100)
+                Text("Capture 1000 Packets").tag(1000)
+                Text("Capture Infinite Packets").tag(0)
             }
-            
+            TextField("Capture Filter", text: $captureFilter)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text(error)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Stop") {
+                self.stopButtonPress()
+            }
+            Button("Erase Frames") {
+                self.frames = []
+            }
         }
     }
     
     func startButtonPress() {
+        self.error = ""
         let interface = "en0"
-        let packetCount: Int32 = 10
-        let expression = "icmp or icmp6"
+        let packetCount: Int32 = Int32(numberPackets)
         let snaplen = 96
         let promiscuousMode = true
         do {
-            etherCapture = try EtherCapture(interface: interface, count: packetCount, command: expression, snaplen: snaplen, promiscuous: promiscuousMode) { frame in
+            etherCapture = try EtherCapture(interface: interface, count: packetCount, command: captureFilter, snaplen: snaplen, promiscuous: promiscuousMode) { frame in
                 self.frames.append(frame)
             }
         } catch {
-            print("\(error)")
+            self.error = error.localizedDescription
         }
+    }
+    func stopButtonPress() {
+        self.error = ""
+        etherCapture?.cancel()
     }
 }
 
