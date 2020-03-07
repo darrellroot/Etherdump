b8ca1e0248180a50eea1ded9477d131fa69e8566
Darrell Root <darrellrootjunk@mac.com>
2020-03-07 10:41:26 -0800

    ARP preview working

--- Etherdump/ContentView/Layer3Views/ArpDetailView.swift
+++ Etherdump/ContentView/Layer3Views/ArpDetailView.swift
@@ -34,8 +34,16 @@ struct ArpDetailView: View {
     }
 }
 
-/*struct BpduDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        BpduDetailView()
+struct ArpDetailView_Previews: PreviewProvider {
+    static var makeArp: Arp {
+        let packetStream = "ffffffffffff685b35890a0408060001080006040001685b35890a04c0a8000a000000000000c0a8000b"
+        let data = Frame.makeData(packetStream: packetStream)!
+        let frame = Frame(data: data, timeval: timeval(), originalLength: 42)
+        guard case .arp(let arp) = frame.layer3 else { fatalError() }
+        return arp
     }
-}*/
+    
+    static var previews: some View {
+        ArpDetailView(arp: ArpDetailView_Previews.makeArp)
+    }
+}
