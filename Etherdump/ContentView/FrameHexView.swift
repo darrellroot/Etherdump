0726631f706efba86ae606891d5f1ed5d7732bc8
Darrell Root <darrellrootjunk@mac.com>
2020-03-15 12:33:43 -0700

    Changed color of highlighted octets

--- Etherdump/ContentView/FrameHexView.swift
+++ Etherdump/ContentView/FrameHexView.swift
@@ -23,23 +23,23 @@ struct FrameHexView: View {
             let dataIndex = position + frame.data.startIndex  //startIndex is not 0 if imported from pcap file
             switch (position % 2 == 0, position % 16 == 0, position % 16 == 15, dataIndex >= start, dataIndex < end) {
             case (false, false, false, true, true): // odd positions
-                retval = retval + Text(datum.plainhex).fontWeight(.bold) + Text(" ")
+                retval = retval + Text(datum.plainhex).foregroundColor(.blue).fontWeight(.bold) + Text(" ")
             case (false, false, false, _, _): // odd positions
                 retval = retval + Text(datum.plainhex) + Text(" ")
             case (false, false, true, true, true): // end of line, odd
-                retval = retval + Text(datum.plainhex).fontWeight(.bold) + Text("\n")
+                retval = retval + Text(datum.plainhex).foregroundColor(.blue).fontWeight(.bold) + Text("\n")
             case (false, false, true, _ , _): // end of line, odd
                 retval = retval + Text(datum.plainhex) + Text("\n")
             case (true, true, false, true, true):  // beginning of line, even
                 retval = retval + Text(position.hex4)
                 retval = retval + Text(" ")
-                retval = retval + Text(datum.plainhex).fontWeight(.bold)
+                retval = retval + Text(datum.plainhex).foregroundColor(.blue).fontWeight(.bold)
             case (true, true, false, _ , _):  // beginning of line, even
                 retval = retval + Text(position.hex4)
                 retval = retval + Text(" ")
                 retval = retval + Text(datum.plainhex)
             case (true, false, false, true, true): // even but not beginning of line
-                retval = retval + Text(datum.plainhex).fontWeight(.bold)
+                retval = retval + Text(datum.plainhex).foregroundColor(.blue).fontWeight(.bold)
             case (true, false, false, _ , _): // even but not beginning of line
                 retval = retval + Text(datum.plainhex)
             case (false, true, false, _ , _),(false, true, true, _ , _),(true, false, true, _ , _),(true, true, true, _ , _):  // invalid cases
